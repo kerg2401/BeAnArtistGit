f164e22ff5c05bc67681f09f099378d528a377b7
Anaïs <Anais.Crosnier@USherbrooke.ca>
2018-08-02 11:00:09 -0400

    knadsjaz

--- BeAnArtistComplet/BeAnArtistComplet/src/fr/eseo/gpi/beanartist/controleur/actions/ActionChoisirCouleur.java
+++ BeAnArtistComplet/BeAnArtistComplet/src/fr/eseo/gpi/beanartist/controleur/actions/ActionChoisirCouleur.java
@@ -12,6 +12,7 @@ public class ActionChoisirCouleur extends AbstractAction {
   
   public ActionChoisirCouleur() {
     super(NOM_ACTION);
+    //dsoijozijf
   }
   
   public void actionPerformed(ActionEvent e) {
