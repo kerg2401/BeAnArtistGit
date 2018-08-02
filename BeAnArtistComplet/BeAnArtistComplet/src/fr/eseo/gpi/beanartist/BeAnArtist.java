ec6f2328712203c7f43e16f4108fee2dcb8edf69
kerg2401 <Gael.Keroyant@USherbrooke.ca>
2018-08-02 17:21:17 +0200

    hettt

--- BeAnArtistComplet/BeAnArtistComplet/src/fr/eseo/gpi/beanartist/BeAnArtist.java
+++ BeAnArtistComplet/BeAnArtistComplet/src/fr/eseo/gpi/beanartist/BeAnArtist.java
@@ -8,7 +8,7 @@ public class BeAnArtist {
   public BeAnArtist() {
     FenetreBeAnArtist f = FenetreBeAnArtist.getInstance();
     f.setVisible(true);
-    //BOnjoureff
+    //BOnjoureffs
   }
 
   public static void main(String[] args) {
